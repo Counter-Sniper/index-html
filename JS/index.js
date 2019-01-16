a6a306a3546ade1c9431c39333755d94224fe175
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-01-16 11:44:13 +0100

    3_4 dołączanie javascriptu do strony

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+/*
+
+Index.js
+
+*/
+
+var komunikat = "Witaj JavaScript";
+console.log(komunikat);
+
+var resultsDiv = document.getElementById("results");
+resultsDiv.innerHTML = "<p>To jest JavaScript</p>";
