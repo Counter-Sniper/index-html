9cd29c27446afec7db0b27d9886be350361292d2
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-01-22 12:30:59 +0100

    4_8 praca z formularzem

--- JS/index.js
+++ JS/index.js
@@ -3,57 +3,88 @@
 Index.js
 
 */
-$(document).ready(function () {
+$(document).ready(function() {
+  "use strict";
 
-    "use strict"
+  var resultsList = $("#resultsList");
+  resultsList.text("To jest z jQuery");
 
-    var resultsList = $("#resultsList");
-    resultsList.text("To jest z jQuery");
+  var toggleButton = $("#toglleButton");
+  toggleButton.on("click", function() {
+    resultsList.toggle(500);
 
-    var toggleButton = $("#toglleButton");
-    toggleButton.on("click", function () {
-        resultsList.toggle(500);
+    if (toggleButton.text() == "Ukryj") toggleButton.text("Pokaż");
+    else toggleButton.text("Ukryj");
+  });
 
-        if (toggleButton.text() == "Ukryj")
-            toggleButton.text("Pokaż");
-        else
-            toggleButton.text("Ukryj");
-    });
+  var listaElementow = $("header nav li");
+  listaElementow.css("font-weight", "bold");
+  listaElementow.css("font-size", "16px");
+  listaElementow.filter(":first").css("font-size", "20px");
 
-    var listaElementow = $("header nav li");
-    listaElementow.css("font-weight", "bold");
-    listaElementow.css("font-size", "16px");
-    listaElementow.filter(":first").css("font-size", "20px");
+  $("#gitHubWynikiForm").on("submit", function() {
+    var szukanaFraza = $("#frazaWyszukiwania").val();
+    var maGwiazdki = $("#maGwiazdki").val();
+    var wybierzJezyk = $("#wybierzJezyk").val();
 
+    if (szukanaFraza) {
+      resultsList.text("Wykonywanie wyszukiwania");
 
-    var gitHubWyniki = "https://api.github.com/search/repositories?q=angular+language:javascript&sort=stars";
+      var gitHubWyniki =
+        "https://api.github.com/search/repositories?q=" +
+        encodeURIComponent(szukanaFraza);
 
-    $.get(gitHubWyniki, function (wynik) {
+      if (wybierzJezyk != "Wszystkie") {
+        gitHubWyniki += "language:" + encodeURIComponent(wybierzJezyk);
+      }
+
+      if (maGwiazdki) {
+        gitHubWyniki += "&sort=stars";
+      }
+
+      $.get(gitHubWyniki, function(wynik) {
         wyswietlanieWynikow(wynik.items);
-    });
-
-    function wyswietlanieWynikow(wynik) {
-        resultsList.empty();
-        $.each(wynik, function (i, element) {
-
-            var newResult = $("<div class='result'>" +
-                "<div class='title'>" + element.name + "</div>" +
-                "<div class='title'>" + element.full_name + "</div>" +
-                "<div class='title'>" + element.description + "</div>" +
-                "<div>Język: " + element.language + "</div>" +
-                "<div>Wlasciciel: " + element.owner.login + "</div>" +
-                "</div>");
-
-            newResult.hover(function () {
-                // przyciemniamy
-                $(this).css("background-color", "lightgray");
-            }, function () {
-                // rozjasniamy
-                $(this).css("background-color", "transparent");
-            });
-
-            resultsList.append(newResult);
-        });
+      });
     }
 
+    return false;
+  });
+
+  function wyswietlanieWynikow(wynik) {
+    resultsList.empty();
+    $.each(wynik, function(i, element) {
+      var newResult = $(
+        "<div class='result'>" +
+          "<div class='title'>" +
+          element.name +
+          "</div>" +
+          "<div class='title'>" +
+          element.full_name +
+          "</div>" +
+          "<div class='title'>" +
+          element.description +
+          "</div>" +
+          "<div>Język: " +
+          element.language +
+          "</div>" +
+          "<div>Wlasciciel: " +
+          element.owner.login +
+          "</div>" +
+          "</div>"
+      );
+
+      newResult.hover(
+        function() {
+          // przyciemniamy
+          $(this).css("background-color", "lightgray");
+        },
+        function() {
+          // rozjasniamy
+          $(this).css("background-color", "transparent");
+        }
+      );
+
+      resultsList.append(newResult);
+    });
+  }
 });
